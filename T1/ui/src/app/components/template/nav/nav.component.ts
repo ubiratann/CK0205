ff3f41c24877b67afc97c11c02cc1f7785c07a28
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-01 14:50:27 -0300

    refactor: implementa menu com material expansions

--- T1/ui/src/app/components/template/nav/nav.component.ts
+++ T1/ui/src/app/components/template/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoaderService } from '@app/utils/loader/loader.service';
 
 @Component({
@@ -11,14 +12,53 @@ export class NavComponent implements OnInit {
   showSubSubMenu: boolean = false;
   isLoggedIn: boolean = false;
 
-  constructor( public loaderService: LoaderService) { }
+  menuList = [
+    {
+      "text": "Perfil",
+      "icon": "person",
+      "public": false,
+      "admin_only": false,
+      "children": [{
+          "text": "Editar",
+          "icon": "manage_accounts",
+          "routerLink": "/editar-perfil",
+        },
+        {
+          "text": "Logout",
+          "icon": "logout",
+          "routerLink": "/logout",
+        }
+      ]
+    },
+    {
+      "text": "Patrimônios",
+      "icon": "category",
+      "public": true,
+      "admin_only": false,
+      "children": [{
+        "text": "Pesquisar",
+        "icon": "search",
+        "routerLink": "/pesquisar-patrimonios",
+        },
+        {
+        "text": "Cadastrar",
+        "icon": "add",
+        "routerLink": "/cadastrar-patrimonio",
+      }]
+    }]
+
+  constructor( 
+    public loaderService: LoaderService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     let test = JSON.parse(localStorage.getItem("user") ?? "") 
 
     if (test) {
       this.isLoggedIn = true
-    }  
+    }
   }
 
 }
+   
