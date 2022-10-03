a354b307e69838dc3ae5286b30317d6430a807c6
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-03 20:44:02 -0300

    feat: implementa menu dinamico

--- T1/ui/src/app/components/template.service.ts
+++ T1/ui/src/app/components/template.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class TemplateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private _updatemenu = new Subject<void>();
+
+  get updateMenu(): Subject<void>{
+    return this._updatemenu
+  }
+
+  get token(): string | null{
+    return localStorage.getItem('token' || '');
+  }
+
+  get role(): string | null{
+    return localStorage.getItem('role' || '');
+  }
+
+  getMenu(role: number){
+    return this.http.get(`${environment.apiUrl}/menu/${role}`);
+  }
+ 
 }
