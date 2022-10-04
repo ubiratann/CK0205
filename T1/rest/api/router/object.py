e98895fddab24c596eb7c6af53eed9564f1285db
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-04 18:20:29 -0300

    fix: adicionar cors e boto3

--- T1/rest/api/router/object.py
+++ T1/rest/api/router/object.py
@@ -1,24 +1,34 @@
 from crypt import methods
 from http import HTTPStatus
+import os
 from flask import Blueprint, Response, request
-
+from flask_cors import CORS
 from api.service.mysql_connector import DatabaseConnector
+
+import json
+import boto3
+
+
+ACESS_KEY_ID = os.environ.get("ACESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 blueprint = Blueprint("object", __name__)
 
+CORS(blueprint)
+
 @blueprint.route("/", methods=["POST"])
 def create():
-    print("entrou")
-    req = request.json
-    connection = DatabaseConnector()
+ 
 
+    # return Response(response=json.dumps({'message':'sucess'}), status=HTTPStatus.CREATED)
     try:
+        
         cursor = connection.get_cursor()
         cursor.execute(f"""
             INSERT INTO SVP.OBJECT (name, location, file, owner ) VALUES(
                 {req.name},
                 {req.location},
-                {req.file},
+                {req.file.name},
                 {req.owner}
             );
         """)
