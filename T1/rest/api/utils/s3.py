8e0e0998ead70fa042777739c12b96358478ed83
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-04 18:19:59 -0300

    feat: adicionar controller do s3

---
+++ T1/rest/api/utils/s3.py
@@ -0,0 +1,60 @@
+import logging
+import boto3
+import base64
+import random
+
+from botocore.exceptions import ClientError
+
+seed(1)
+
+def create_local_temp_file(base64, filename, username):
+    suffix = random
+
+def delete_local_temp_file(filePath):
+    ...
+    
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        raise e
+    return True
+
+
+def upload_file(file_name, bucket, object_name):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        return response
+    
+    except ClientError as e:
+        logging.error(e)
+        return False
