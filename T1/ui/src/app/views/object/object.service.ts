408cc4910ca7c27fa6314b5a0566e1bc422c3b3c
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-10-05 18:54:44 -0300

    feat: implementa pipe com pop de erros

--- T1/ui/src/app/views/object/object.service.ts
+++ T1/ui/src/app/views/object/object.service.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '@environments/environment';
+import { Object } from '@app/models/object';
+import { catchError, throwError } from 'rxjs';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { SnackbarService } from '@app/utils/snackbar/snackbar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ObjectService {
-
-  constructor(private http: HttpClient) { }
+  
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  
+  
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private snackBarService: SnackbarService) { }
 
   create(obj: any){
-    return this.http.post<any>(`${environment.apiUrl}/object/`, obj);
+    return this.http.post<any>(`${environment.apiUrl}/object/`, obj)
+      .pipe(
+        catchError(this.handler.bind(this))
+      );
+  }
+
+  get(id: any){
+    return this.http.get<any>(`${environment.apiUrl}/object/${id}`)
+      .pipe(
+        catchError(this.handler.bind(this))
+      );
+  }
+
+  handler(error: any){
+
+    this.snackBarService.openSnackBar(error.error.message, 'fechar');
+    return throwError(() => error)
   }
 
 }
