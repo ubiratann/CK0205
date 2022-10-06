153d29be8d679062a57b57d1a367ef0542f74104
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-10-06 10:19:57 -0300

    feat: implementa integracao com rest

--- T1/ui/src/app/views/user/user-objects/user-objects.component.ts
+++ T1/ui/src/app/views/user/user-objects/user-objects.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ObjectService } from '@app/views/object/object.service';
+import { UserReportsComponent } from '../user-reports/user-reports.component';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-objects',
@@ -6,29 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-objects.component.css']
 })
 export class UserObjectsComponent implements OnInit {
-  resultList: Object[] =  [
-    {
-      name: 'teste',
-      location: 'teste',
-      id: 1,
-      file: 'teste',
-      owner_id: 1,
-    },
-    {
-      name: 'Lorem ipsum dolor sit amet consectetur adipisicing elite?',
-      location: 'Lorem ipsum dolor sit amet ',
-      id: 2,
-      file: 'teste',
-      validated: true,
-      owner_id: 2
-    }
-  ];
+  resultList: [] = []
 
   displayedColumns = ["id", "name", "location", "validated", "open"]
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private service: ObjectService) { }
 
   ngOnInit(): void {
+    this.service.getReports(2)
+    .subscribe(data => {
+      console.log(data)
+      this.resultList = data.data;
+    })
+  }
+
+  showReports(validations: any){
+    console.log(validations.resume)
+    
+    const dialogRef = this.dialog.open(UserReportsComponent, {
+      minWidth: "600px",
+      data: { 
+       validations: validations.resume
+    }});
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 
 }
