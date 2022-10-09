db0c93b74ba312f9d5e4fb22c65b40f7d13c5c0a
Geovane Oliveira <geoliiveira2@gmail.com>
2022-10-09 00:45:02 -0300

    fix: corrige endpoint para atualização de informações do usuário

--- T1/rest/api/router/user.py
+++ T1/rest/api/router/user.py
@@ -48,30 +48,46 @@ def auth():
 
     return Response(response={"message": msg}, status=code)
 
-@blueprint.patch("/")
-def update():
+@blueprint.patch("/<int:id>")
+def update(id):
     req = request.json
     db = conn.get_cursor()
 
-    fullname = req['full_name']
-    username = req['username']
-    password = req['password']
     try:
-        _hashed_password = generate_password_hash(password)
-        db.execute(f"UPDATE users SET full_name='{fullname}', username='{username}', password='{_hashed_password}'")
-        conn.commit_changes()
+        db.execute("SELECT id FROM users WHERE id=%s", (id,))
+        user = db.fetchone()
 
-        msg = "Deletado com sucesso"
-        code = HTTPStatus.NO_CONTENT
+        if user is not None:
+            db.execute("SELECT * FROM users WHERE username = %s", (req['username'],))
+            user = db.fetchone()
+
+            if user is None or (user is not None and user['username'].lower() == req['username'].lower() and user['id'] == id):
+                db.execute(
+                    "UPDATE users SET full_name=%s, username=%s, password=%s WHERE id=%s",
+                    (
+                        req['full_name'],
+                        req['username'],
+                        generate_password_hash(req['password']),
+                        id,
+                    )
+                )
+                msg = "Alterado com sucesso"
+                code = HTTPStatus.OK
+            else:
+                msg = "Username já existe"
+                code = HTTPStatus.NOT_ACCEPTABLE
+            
+        else:
+            msg = "Usuario não encontrado"
+            code = HTTPStatus.NOT_FOUND
+
+        db.close() 
     except Exception as err:
         logging.error(err)
         msg = err
         code = HTTPStatus.INTERNAL_SERVER_ERROR
-    finally:
-        db.close() 
-        conn.close_connection()
 
-    return Response(response={"message": msg}, status=code)
+    return Response(msg, status=code)
 
 
 @blueprint.delete('/delete/<int:id>')
@@ -113,7 +129,7 @@ def register():
                 "INSERT INTO users (full_name, username, password, role) VALUES (%s, %s, %s, %s)", 
                 (
                     req['full_name'],
-                    req['username'],
+                    req['username'].lower(),
                     generate_password_hash(req['password']),
                     req['role'],
                 )
