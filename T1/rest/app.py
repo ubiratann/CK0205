a50fac6b711c2f5e57dcf195777f3a2eaf6e8479
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-10 20:27:14 -0300

    feat: adiciona rota de menu

--- T1/rest/app.py
+++ T1/rest/app.py
@@ -1,6 +1,8 @@
 import os
-import api.router.object as object
+import api.router.object as obj
 import api.router.user as user
+import api.router.menu as menu
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,5 +14,6 @@ if(ENVIRONMENT == "DEV"):
 
 version = os.environ.get("API_VERSION", "v1")
 
-app.register_blueprint(object.blueprint, url_prefix=f"/api/{version}/object")
+app.register_blueprint(obj.blueprint, url_prefix=f"/api/{version}/object")
 app.register_blueprint(user.blueprint, url_prefix=f"/api/{version}/user")
+app.register_blueprint(menu.blueprint, url_prefix=f"/api/{version}/menu")
