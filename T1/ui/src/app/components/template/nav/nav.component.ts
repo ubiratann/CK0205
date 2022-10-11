6d0b5d1450ef237bb2c58fe1c4028b4404abbe13
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-10-11 01:07:20 -0300

    feat: implementa menu dinamico

--- T1/ui/src/app/components/template/nav/nav.component.ts
+++ T1/ui/src/app/components/template/nav/nav.component.ts
@@ -13,82 +13,37 @@ export class NavComponent implements OnInit {
 
   showSubSubMenu: boolean = false;
   isLoggedIn: boolean = false;
-  menuList: any = [
-    {
-      "text": "Home",
-      "icon": "home",
-      "public": false,
-      "admin_only": false,
-      "routerLink": "/",
-    },
-    {
-      "text": "Perfil",
-      "icon": "person",
-      "public": false,
-      "admin_only": false,
-      "children": [{
-          "text": "Editar",
-          "icon": "manage_accounts",
-          "routerLink": "/editar-perfil",
-        },
-        {
-          "text": "Logout",
-          "icon": "logout",
-          "routerLink": "/logout",
-        }
-      ]
-    },
-    {
-      "text": "Patrimônios",
-      "icon": "category",
-      "public": true,
-      "admin_only": false,
-      "children": [{
-        "text": "Pesquisar",
-        "icon": "search",
-        "routerLink": "/pesquisar-patrimonios",
-        },
-        {
-        "text": "Cadastrar",
-        "icon": "add",
-        "routerLink": "/cadastrar-patrimonio",
-      }]
-    },
-    {
-      "text": "Admin",
-      "icon": "admin_panel_settings",
-      "public": true,
-      "admin_only": false,
-      "children": [{
-        "text": "Usuários",
-        "icon": "group",
-        "routerLink": "/gerenciar-usuarios",
-        }]
-    }
-  ];  
+  menuList: any = [];  
 
+  role: any;
 
   constructor( 
     public loaderService: LoaderService,
     private templateService: TemplateService,
     private userService: UserService,
     private router: Router,
-  ) { }
+  ) {
+    this.loaderService.isLoading.next(true)
+   }
 
 
   ngOnInit(): void {
-    // this.loadMenu();
 
-    // this.templateService.updateMenu.subscribe(data => {
-    //   this.loadMenu();
-    // });
+    this.role = Number.parseInt(localStorage.getItem("user_role" ) || "1");
+
+    this.loadMenu();
+
+    this.templateService.updateMenu.subscribe(data => {
+      this.loadMenu();
+    });
   }
   
   loadMenu(){
     if(this.userService.token != ''){
-      this.menuList = this.templateService.getMenu(this.userService.role)
-        .subscribe(data => {
-          this.menuList = data;
+      this.menuList = this.templateService.getMenu(this.role)
+        .subscribe((data: any) => {
+          console.log(data)
+          this.menuList = data.data ;
         })
     }
   }
