2604d4818fc3f16366adfb88843134e88eddebda
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-11-01 00:25:47 -0300

    fix: corrige link de retorno do arquivo enviado

--- T2/rest/api/utils/minio.py
+++ T2/rest/api/utils/minio.py
@@ -35,7 +35,7 @@ def create_bucket(bucket_name):
 
 def upload_file(object_name, bucket_name):
     minio_client.fput_object(bucket_name=bucket_name, object_name=object_name, file_path=f"/tmp/{object_name}")
-    return f"{MINIO_HOST}/{bucket_name}/{object_name}"
+    return f"http://{MINIO_HOST}:{MINIO_HOST_PORT}/{bucket_name}/{object_name}"
 
 def delete_file(bucket, object_name):
     minio_client.remove_object(bucket_name=bucket, object_name=object_name)
