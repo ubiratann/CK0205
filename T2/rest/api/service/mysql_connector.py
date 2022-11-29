859cbba5e94e0fb9acba14c52faf72ee9e93effb
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-11-29 18:27:41 +0000

    feat: implementa reconexao

--- T2/rest/api/service/mysql_connector.py
+++ T2/rest/api/service/mysql_connector.py
@@ -12,18 +12,24 @@ DATABASE = os.environ.get("MYSQL_DATABASE", "svp")
 
 class DatabaseConnector:
 
-    def __init__(self) -> None:  
-        try:
-            self.connection: MySQLConnection = mysql.connector.connect(host=HOST,
-                                                                       database=DATABASE,
-                                                                       user=USER,
-                                                                       password=PASSWORD,
-                                                                       autocommit=True)
-        except Error as err:
-            logging.error(err)
+    def __init__(self) -> None:
+        self.connection = None  
+        self.get_connection()
 
     def get_cursor(self) -> MySQLCursor:
+        self.get_connection()
         return self.connection.cursor(dictionary=True)
+
+    def get_connection(self):
+        if not self.connection:
+            try:
+                self.connection: MySQLConnection = mysql.connector.connect(host=HOST,
+                                                                        database=DATABASE,
+                                                                        user=USER,
+                                                                        password=PASSWORD,
+                                                                        autocommit=True)
+            except Error as err:
+                logging.error(err)
 
     def close_connection(self) -> None:
         self.connection.close()
